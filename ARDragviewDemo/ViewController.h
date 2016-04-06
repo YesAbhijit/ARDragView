6ab27f69651773699d10463cd2043c7c77e0f21c
ABHIJIT <rana.the.abhijit@gmail.com>
2016-04-06 13:15:04 +0530

    Initial Commit

---
+++ ARDragviewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARDragviewDemo
+//
+//  Created by AbhijitRana on 06/04/16.
+//  Copyright © 2016 AbhijitRana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ARDragviewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ARDragviewDemo
+//
+//  Created by AbhijitRana on 06/04/16.
+//  Copyright © 2016 AbhijitRana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
